dce0bc9e316bc500b58cb0b6feea80921fbadb2f
Fernando Martins <fmbr.martins@gmail.com>
2026-07-24 13:36:12 +0200

    Add alternate visual style toggle

---
+++ visual-style.js
@@ -0,0 +1,11 @@
+'use strict';
+(() => {
+ let anime=false;
+ function apply(){document.body.dataset.visualStyle=anime?'anime':'classic'}
+ function toggle(){anime=!anime;apply();return anime}
+ window.VISUAL_STYLE=Object.freeze({isAnime:()=>anime,toggle});
+ addEventListener('keydown',event=>{
+  if(event.code==='KeyK'&&!event.repeat){event.preventDefault();toggle()}
+ });
+ apply();
+})();
